72c59f2abc0a111459457e1aa024085615c42c1d
papercoding22 <john.ly997@gmail.com>
2024-11-24 14:21:16 +0700

    add: config management module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { TaskRepository } from "./dto/task.repository";
 import { Task } from "./dto/task.entity";
 import { AuthModule } from "src/auth/auth.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
+  imports: [ConfigModule, TypeOrmModule.forFeature([Task]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService, TaskRepository],
 })
